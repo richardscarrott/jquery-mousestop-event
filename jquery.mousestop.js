8bed3ca2fa0ee9837bed1c8a91321ad7067d4a5e
Richard Scarrott <riscarrott@googlemail.com>
2013-01-20 22:32:13 +0000

    Prevented mousestop from firing multiple times within same element.

--- jquery.mousestop.js
+++ jquery.mousestop.js
@@ -1,9 +1,9 @@
 /*
- * jQuery Mousestop Event v1.0
+ * jQuery Mousestop Event v0.1.0
  * http://richardscarrott.co.uk/posts/view/jquery-mousestop-event
  *
  * Copyright (c) 2010 Richard Scarrott
- * W/ thanks to Ben Alman for his decent jQuery special event API write up:
+ * W/ thanks to Ben Alman for his jQuery special event API write up:
  * http://benalman.com/news/2010/03/jquery-special-events/
  *
  * Dual licensed under the MIT and GPL licenses:
@@ -15,53 +15,54 @@
  */
 
 (function($) {
-	// public vars
-	$.mousestopDelay = 50;
+    // public vars
+    $.mousestopDelay = 50;
 
-	// special event
-	$.event.special.mousestop = {
-		setup: function(data) {
-			$(this).data('mousestop', {delay: data})
-				   .bind('mouseenter.mousestop', mouseenterHandler)
-				   .bind('mouseleave.mousestop', mouseleaveHandler);
-		},
-		teardown: function() {
-			$(this).removeData('mousestop')
-			       .unbind('.mousestop');
-		}
-	};
+    // special event
+    $.event.special.mousestop = {
+        setup: function(data) {
+            $(this).data('mousestop', {delay: data})
+                   .bind('mouseenter.mousestop', mouseenterHandler)
+                   .bind('mouseleave.mousestop', mouseleaveHandler);
+        },
+        teardown: function() {
+            $(this).removeData('mousestop')
+                   .unbind('.mousestop');
+        }
+    };
 
-	// private methods
-	function mouseenterHandler() {
-		if (typeof this.timeout === 'undefined') {
-			this.timeout = null;
-		}
-		
-		var elem = $(this),
-			data = elem.data('mousestop'),
-			delay = data.delay || $.mousestopDelay;
+    // private methods
+    function mouseenterHandler() {
+        if (typeof this.timeout === 'undefined') {
+            this.timeout = null;
+        }
+        
+        var elem = $(this),
+            data = elem.data('mousestop'),
+            delay = data.delay || $.mousestopDelay;
 
-		elem.bind('mousemove.mousestop', function() {
-			clearTimeout(this.timeout);
-			this.timeout = setTimeout(function() {
-				elem.trigger('mousestop');
-			}, delay);
-		});
-	};
-	
-	function mouseleaveHandler() {
-		var elem = $(this);
-		elem.unbind('mousemove.mousestop');
-		clearTimeout(this.timeout);
-	};
+        elem.bind('mousemove.mousestop', function() {
+            clearTimeout(this.timeout);
+            this.timeout = setTimeout(function() {
+                elem.trigger('mousestop');
+                elem.unbind('mousemove.mousestop');
+            }, delay);
+        });
+    };
+    
+    function mouseleaveHandler() {
+        var elem = $(this);
+        elem.unbind('mousemove.mousestop');
+        clearTimeout(this.timeout);
+    };
 
-	// shorthand alias
-	$.fn.mousestop = function(data, fn) {
-		if (fn == null) {
-			fn = data;
-			data = null;
-		}
+    // shorthand alias
+    $.fn.mousestop = function(data, fn) {
+        if (fn == null) {
+            fn = data;
+            data = null;
+        }
 
-		return arguments.length > 0 ? this.bind('mousestop', data, fn) : this.trigger('mousestop');
-	};
+        return arguments.length > 0 ? this.bind('mousestop', data, fn) : this.trigger('mousestop');
+    };
 })(jQuery);
